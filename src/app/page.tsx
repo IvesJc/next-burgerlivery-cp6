ac0d034a868ff063692efeec4946696bbeed6cbc
Ives Chiba <ivesjundi@gmail.com>
2024-05-21 12:03:12 -0300

    fix :bug: .git in wrong file

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hamburgers para todos os gostos</h1>
+      <p>Nós fazemos deliciosos hamburgers e entregamos na sua porta</p>
+    </div>
+  );
+}
